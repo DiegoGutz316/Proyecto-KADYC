c22d813f46f73e1319ada6560eb1c31ea8fe764b
Diego Gutierrez <luispc135579@gmail.com>
2025-11-10 16:27:09 -0600

    Aviso de cerrar sesion

--- Data/ApplicationDBContext.cs
+++ Data/ApplicationDBContext.cs
@@ -14,7 +14,6 @@ namespace Proyecto_v1.Data
         public DbSet<Order> Orders { get; set; }
         public DbSet<OrderDetail> OrderDetails { get; set; }
         public DbSet<Product> Products { get; set; }
-        public DbSet<Shipping> Shippings { get; set; }
         public DbSet<Cart> Carts { get; set; }
         public DbSet<CartItem> CartItems { get; set; }
 
